e6c3b2d6d77a8b98cf101458cd5b39ec29332624
Linegrind <linegrind@icloud.com>
2021-05-04 16:13:38 -0600

    user login get and post requests

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const mysql = require('mysql2');
 const Handlebars = require('handlebars');
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({});
+const passport = require('passport');
 
+// Server port
 const PORT = process.env.PORT || 3001;
+
+// App creation
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+// App parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// App passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// App handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
